c7839dc322085d9f693b7f4d93a7f3b89c2d94a1
Abir <abirhachlafi1@gmail.com>
2026-07-25 01:55:23 +0100

    Add temporary debug endpoint to retrieve decrypted Shopify access token from production DB

--- products/urls.py
+++ products/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from products.views import ProductViewSet, ShopifyCallbackView
+from products.views import ProductViewSet, ShopifyCallbackView, ShopifyDebugTokenView
 
 router = DefaultRouter()
 router.register(r'', ProductViewSet, basename='products')
 
 urlpatterns = [
     path('callback/', ShopifyCallbackView.as_view(), name='shopify_callback'),
+    path('debug-token/', ShopifyDebugTokenView.as_view(), name='shopify_debug_token'),
     path('', include(router.urls)),
 ]
 
